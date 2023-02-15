f6d9eff35c6ee53e7c9f18a06868ffebb8063308
Ronin Thatcher <thatcherwill99@gmail.com>
2023-02-15 15:59:48 -0700

    Added Conventions&Syntax Script

---
+++ Spring Class Project 1/Assets/Scripts/Conventions&Syntax.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConventionsandSyntax : MonoBehaviour
+{
+    void Start()
+    {
+        //this line is there to tell me the x position of my object
+
+        /*Hi There!
+         * This is two lines!
+         * */
+        Debug.Log(transform.position.x);
+
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to hit the ground!");
+        }
+    }
+}
