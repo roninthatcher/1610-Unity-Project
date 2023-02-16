f3a5219ce5ab0aa2bd5c7acdf8ba092fc78a2fe7
Ronin Thatcher <thatcherwill99@gmail.com>
2023-02-15 17:17:51 -0700

    Added Using Scripts Practice Script

---
+++ Spring Class Project 1/Assets/Scripts/UsingScriptsPractice.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingScriptsPractice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am Alive!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
