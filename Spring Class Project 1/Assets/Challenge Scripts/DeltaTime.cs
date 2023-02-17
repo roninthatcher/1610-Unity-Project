f853c2ad1503cd26fc0a92c7f59045952847e93b
Ronin Thatcher <thatcherwill99@gmail.com>
2023-02-17 11:36:55 -0700

    Added Delta Time Script

---
+++ Spring Class Project 1/Assets/Challenge Scripts/DeltaTime.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeltaTime : MonoBehaviour
+{
+    public float speed = 8f;
+    public float countdown = 3.0f;
+
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0.0f)
+            light.enabled = true;
+
+        if (Input.getKey(KeyCode.RightArrow))
+            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
