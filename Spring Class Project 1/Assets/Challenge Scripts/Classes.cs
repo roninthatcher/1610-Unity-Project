08298e20c8089134c7232c568bf56fc9d6ded654
Ronin Thatcher <thatcherwill99@gmail.com>
2023-02-17 11:49:28 -0700

    Added Classes Script

---
+++ Spring Class Project 1/Assets/Challenge Scripts/Classes.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleCharacterScript : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int bullets;
+        public int grenades;
+        public int rockets;
+
+        public Stuff(int bul, int gre, int roc)
+        {
+            bullets = bul;
+            grenades = gre;
+            rockets = roc;
+        }
+    }
+
+
+    public Stuff myStuff = new Stuff(10, 7, 25);
+    public float speed;
+    public float turnSpeed;
+    public Rigidbody bulletPrefab;
+    public Transform firePosition;
+    public float bulletSpeed;
+
+
+    void Update()
+    {
+        Movement();
+        Shoot();
+    }
+
+
+    void Movement()
+    {
+        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * forwardMovement);
+        transform.Rotate(Vector3.up * turnMovement);
+    }
+
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && myStuff.bullets > 0)
+        {
+            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            bulletInstance.AddForce(firePosition.forward * bulletSpeed);
+            myStuff.bullets--;
+        }
+    }
+}
+
+public class Inventory : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int bullets;
+        public int grenades;
+        public int rockets;
+        public float fuel;
+
+        public Stuff(int bul, int gre, int roc)
+        {
+            bullets = bul;
+            grenades = gre;
+            rockets = roc;
+        }
+
+        public Stuff(int bul, float fu)
+        {
+            bullets = bul;
+            fuel = fu;
+        }
+
+        // Constructor
+        public Stuff()
+        {
+            bullets = 1;
+            grenades = 1;
+            rockets = 1;
+        }
+    }
+
+
+    // Creating an Instance (an Object) of the Stuff class
+    public Stuff myStuff = new Stuff(50, 5, 5);
+
+    public Stuff myOtherStuff = new Stuff(50, 1.5f);
+
+    void Start()
+    {
+        Debug.Log(myStuff.bullets);
+    }
+}
+
+public class MovementControls : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+
+
+    void Update()
+    {
+        Movement();
+    }
+
+
+    void Movement()
+    {
+        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * forwardMovement);
+        transform.Rotate(Vector3.up * turnMovement);
+    }
+}
+
+public class Shooting : MonoBehaviour
+{
+    public Rigidbody bulletPrefab;
+    public Transform firePosition;
+    public float bulletSpeed;
+
+
+    private Inventory inventory;
+
+
+    void Awake()
+    {
+        inventory = GetComponent<Inventory>();
+    }
+
+
+    void Update()
+    {
+        Shoot();
+    }
+
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && inventory.myStuff.bullets > 0)
+        {
+            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            bulletInstance.AddForce(firePosition.forward * bulletSpeed);
+            inventory.myStuff.bullets--;
+        }
+    }
+}
