57752b60f2ef965a5d1af9f69759376fedc026fa
Ronin Thatcher <thatcherwill99@gmail.com>
2023-02-16 19:24:24 -0700

    Added Destroy Script

---
+++ Spring Class Project 1/Assets/Challenge Scripts/Destory.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryBasic : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destory(gameObject);
+        }
+    }
+}
+
+public class DestoryOther : MonoBehaviour
+{
+    public GameObject other;
+
+    void Update()
+    {
+        if(Input.GetKay(KeyCode.Space))
+        {
+            Destory(other);
+        }
+    }
+}
+
+public class DestoryCompenent : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKay(KeyCode.Space))
+        {
+            Destory(GetComponent<MeshRenderer>();
+        }
+    }
+}
