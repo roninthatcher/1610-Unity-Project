1e3bea18cc6f7d1f793d1705224bb66377090993
Ronin Thatcher <thatcherwill99@gmail.com>
2023-03-15 14:13:50 -0600

    Added Projectile Scripts

---
+++ UFO Defense Force Game/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed = 50.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move Game Object forwatd
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
--- UFO Defense Force Game/Assets/Scripts/PlayerControls.cs
+++ UFO Defense Force Game/Assets/Scripts/PlayerControls.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed;
+    public float speed = 25;
+    public float xRange = 15;
 
-    void Start()
-    {
-        
-    }
+    public Transform blaster;
+    public GameObject lazerBolt;
 
     void Update()
     {
@@ -19,5 +18,28 @@ public class PlayerControls : MonoBehaviour
 
         //Moves Player left and right
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        //Keeps player within bounds
+        //Left side wall
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        //Right side wall
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        //if space bar is pressed, fire lazerBolt
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Create LazerBolt at the Blaster transform position maintaining the objects rotation.
+            Instantiate(lazerBolt, blaster.transform.position, lazerBolt.transform.rotation);
+        }
+    }
+
+    //Delete any other object with a trigger that hits the player
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
     }
 }
