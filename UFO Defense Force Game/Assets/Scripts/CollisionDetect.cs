09317fe14960f5f4a2e983b295ce2aab2750f94c
Ronin Thatcher <thatcherwill99@gmail.com>
2023-03-22 13:26:57 -0600

    Edited Collision Detect Script

--- UFO Defense Force Game/Assets/Scripts/CollisionDetect.cs
+++ UFO Defense Force Game/Assets/Scripts/CollisionDetect.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class CollisionDetect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(gameObject); //Destory this game object
+        Destroy(other.gameObject); //Destory other game object it hits
     }
 }
