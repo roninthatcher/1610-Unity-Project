1a7a03c0a9e2f0e41e621ec2d6c933ea4d3fd818
Ronin Thatcher <thatcherwill99@gmail.com>
2023-04-05 12:59:48 -0600

    Added Other Matching System Scripts

--- Color Match Game/Assets/Scripts/MatchBehaviour.cs
+++ Color Match Game/Assets/Scripts/MatchBehaviour.cs
@@ -2,13 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MatchBehaviour : MonoBehaviour
 {
     public ID idObj;
+    public UnityEvent matchEvent, noMatchEvent;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(idObj);
+        var tempObj = other.GetComponent<IDContainerBehaviour>();
+        if (tempObj != null)
+            return;
+
+        var otherID = tempObj.idObj;
+        if (otherID == idObj)
+        {
+            matchEvent.Invoke();
+        }
+        else
+        {
+            noMatchEvent.Invoke();
+        }
     }
 }
